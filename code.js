62b9d569b29630631531c920b1dc49f5383ee921
riya9193 <priyanshipanwar348@gmail.com>
2026-02-23 16:20:06 +0530

    add js file

---
+++ code.js
@@ -0,0 +1,61 @@
+
+
+// let mess = "hello";
+// mess = "Java Script";
+// console.log(mess); 
+
+// var a = 10;
+// var a = 20 ; 
+// console.log(a);
+
+// console.log("hello world") ;
+
+// console.log("let introduction") ;
+
+const x = 100 ;
+
+console.log(x);
+
+const info = {
+      
+       name : "priyanshi",
+         age : 20,
+         city : "Baraut"
+        
+        }
+    
+console.log(info.name); 
+
+// arthmetic operator 
+
+let a = 2 ; 
+let b = 3 ; 
+
+console.log(a + b) ; // addition
+console.log(a - b) ; // subtraction
+console.log(a * b) ;    // multiplication       
+console.log(a / b) ;    // division
+console.log(a % b) ; // modulus
+console.log(a ** b) ; // exponentiation
+
+//assignment operator 
+
+let c = 10 ; 
+c += 5 ; // c = c + 5
+console.log(c) ;
+
+c-= 5 ;
+console.log(c) ;
+
+c*= 2 ; 
+
+console.log(c) ;
+
+c/= 5 ;
+console.log(c) ;
+
+c%= 2 ;
+console.log(c) ;
+
+c**= 2 ; 
+console.log(c) ;
