a9fb0f938a78aae6c06b12bcf466cfd3543d7ec6
riya9193 <priyanshipanwar348@gmail.com>
2026-02-24 15:29:05 +0530

    add switch file

--- condition.js
+++ condition.js
@@ -7,23 +7,3 @@ if ( temp>30 ) {
       console.log("it's a cold day") ;
 }
 
-let day = 3 ; 
-
-switch (day) {
-    case 1 : console.log("monday") ;
-    break ;
-    case 2 : console.log("tuesday") ;
-    break ;
-    case 3 : console.log("wednesday") ;
-    break ;
-    case 4 : console.log("thursday") ;
-    break ;
-    case 5 : console.log("friday") ;
-    break ;
-    case 6 : console.log("saturday") ;
-    break ;
-    case 7 : console.log("sunday") ;
-    break ;
-    default : console.log(" invalid day ") ;
-}
-
