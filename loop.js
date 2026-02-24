f524d42436ba42353fe5e513772f85328d7034cd
riya9193 <priyanshipanwar348@gmail.com>
2026-02-24 15:34:36 +0530

    add loop

---
+++ loop.js
@@ -0,0 +1,15 @@
+for (let i = 1; i <= 3; i++) {
+     console.log(i) ;
+}
+
+let j = 4 ;
+while (j <= 6) {
+     console.log(j) ;
+     j++ ;
+}
+
+let k = 7 ;
+do {
+        console.log(k) ;
+        k++ ;
+} while (k <= 10) ;
