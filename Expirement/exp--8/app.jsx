144fffdee1dc944d797c38a7ef7886f482730461
priyanshi_cse <priyanshipanwar348@gmail.com>
2026-05-30 15:51:33 +0100

    exp-8

---
+++ Expirement/exp--8/app.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  return (
+    <>
+      <h1>React Counter Application</h1>
+
+      <h2>{count}</h2>
+
+      <button onClick={increment}>Increment</button>
+      <br /><br />
+
+      <button onClick={decrement}>Decrement</button>
+      <br /><br />
+
+      <button onClick={reset}>Reset</button>
+    </>
+  );
+}
+
+export default App;
