e3ea0c6a8ee13cee64b36b8ab8e2672eae322c16
priyanshi_cse <priyanshipanwar348@gmail.com>
2026-05-30 15:21:35 +0100

    add folder

--- exp-1/code.js
+++ exp-1/code.js
@@ -1,47 +1,31 @@
-
-
 function calculateResult(){
     let n = parseInt(document.getElementById("subject").value);
     let total = 0;
-
     for (let i = 1; i <= n; i++) {
         let marks = parseFloat(prompt("Enter marks for subject " + i));
         if(isNaN(marks)){
-            marks = 0;
-        }
-        total += marks;
-    }
-
+            marks = 0;}
+        total += marks;}
     let avg = total / n;
     let grade;
     let result;
-
     if(avg >= 90){
-        grade = "A++";
-    }
+        grade = "A++";}
     else if(avg >= 80){
-        grade = "A+";
-    }
+        grade = "A+";}
     else if(avg >= 70){
-        grade = "A";
-    }
+        grade = "A";}
     else if(avg >= 60){
-        grade = "B";
-    }
+        grade = "B";}
     else if(avg >= 50){
-        grade = "C";
-    }
+        grade = "C";}
     else{
-        grade = "F";
-    }
-
-    if(avg >= 40){
-        result = "Pass";
-    }else{
-        result = "Fail";
-    }
-
-    document.getElementById("result").innerHTML =
+        grade = "F";}
+     if(avg >= 40){
+        result = "Pass";}
+        else{
+        result = "Fail";}
+   document.getElementById("result").innerHTML =
     "Total Marks : " + total + "<br>" +
     "Average Marks : " + avg.toFixed(2) + "<br>" +
     "Grade : " + grade + "<br>" +
